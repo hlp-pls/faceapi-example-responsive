a7eed17a0652999e9c616170683d4fa5adeaecd3
hlp-pls <53251969+hlp-pls@users.noreply.github.com>
2020-05-14 09:57:42 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -8,15 +8,18 @@ var offset;
 
 let cam_width;
 let cam_height;
+let scale;
+
+console.log(faceapi.nets);
 
 function setup(){
 	createCanvas(windowWidth,windowHeight);
 	noFill();
 	strokeWeight(2);
-	frameRate(30);
+	//frameRate(30);
 	Promise.all([
 	faceapi.nets.tinyFaceDetector.loadFromUri('models/'),
-	faceapi.nets.faceLandmark68Net.loadFromUri('models/'),
+	faceapi.nets.faceLandmark68TinyNet.loadFromUri('models/'),
 	//faceapi.nets.faceRecognition.loadFromUri('/models'),
 	//faceapi.nets.faceExpressionNet.loadFromUri('/models')
 	]).then(modelLoaded);
@@ -24,20 +27,39 @@ function setup(){
 	webcam.hide();
 }
 
+function windowResized(){
+	resizeCanvas(windowWidth,windowHeight);
+}
+
 function modelLoaded(){
 	console.log("model loaded");
 	is_model_loaded = true;
+
+	setInterval(async () => {
+		predict();
+	},100);
 }
 
 function draw(){
 	clear();
-
 	if(width>height){
 		cam_width = height * webcam.width/webcam.height;
 		cam_height = height;
+
 	}else{
 		cam_width = width;
 		cam_height = width * webcam.height/webcam.width;
+
+	}
+
+	if(width>cam_width){
+		scale = width/cam_width;
+		cam_width *= scale;
+		cam_height *= scale;
+	}else if(height>cam_height){
+		scale = height/cam_height;
+		cam_width *= scale;
+		cam_height *= scale;
 	}
 
 	if(is_model_loaded){
@@ -46,8 +68,11 @@ function draw(){
 			width/2-cam_width/2,
 			height/2-cam_height/2,
 			cam_width,cam_height);
+
+		//if(webcam.changed()){
+			
+		//}
 		
-		predict();
 		if(landmark_points){
 			beginShape();
 			for(let i=0; i<landmark_points.length; i++){
@@ -66,12 +91,13 @@ function draw(){
 }
 
 async function predict(){
+	const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 128 })
 	const video = document.getElementsByTagName('video')[0];
 	const displaySize = { width: cam_width, height: cam_height};
 	const detections = await faceapi.detectAllFaces(
 			video,
-			new faceapi.TinyFaceDetectorOptions()
-		).withFaceLandmarks()
+			new faceapi.TinyFaceDetectorOptions(options)
+		).withFaceLandmarks(true)
 		//console.log(detections)
 		//console.log(detections[0].landmarks)
 		const resizedDetections = faceapi.resizeResults(detections,displaySize);
